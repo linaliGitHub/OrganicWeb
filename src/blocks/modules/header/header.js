97eccaee308469030c50e54658381a24b62c7954
Alina <alinakazakbaeva@yandex.ru>
2021-02-26 11:19:36 +0300

    menu

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,7 +1,10 @@
 document.querySelector('.header-menu').onclick = function () {
   document.querySelector('.menu').style.right = '0';
+  document.querySelector('.menu').style.display = 'block';
   document.querySelector('.nav').style.right = '60%';
+  document.querySelector('.nav').style.display = 'block';
   document.querySelector('.close').style.right = '20px';
+  document.querySelector('.close').style.display = 'block';
   document.querySelector('body').classList.add('active');
 };
 document.querySelector('.close').onclick = function () {
