9cdef9385228d7c5b69cbd6487c69735c77b1173
Alina <alinakazakbaeva@yandex.ru>
2021-02-26 21:10:13 +0300

    mobile

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,15 +1,12 @@
 document.querySelector('.header-menu').onclick = function () {
-  document.querySelector('.menu').style.right = '0';
-  document.querySelector('.menu').style.display = 'block';
-  document.querySelector('.nav').style.right = '60%';
-  document.querySelector('.nav').style.display = 'block';
-  document.querySelector('.close').style.right = '20px';
-  document.querySelector('.close').style.display = 'block';
+  document.querySelector('.menu').style.top = '0';
+  document.querySelector('.nav').style.top = '50px';
+  document.querySelector('.close').style.top = '20px';
   document.querySelector('body').classList.add('active');
 };
 document.querySelector('.close').onclick = function () {
-  document.querySelector('.menu').style.right = '-100%';
-  document.querySelector('.nav').style.right = '-100%';
-  document.querySelector('.close').style.right = '-100%';
+  document.querySelector('.menu').style.top = '-100%';
+  document.querySelector('.nav').style.top = '-100%';
+  document.querySelector('.close').style.top = '-100%';
   document.querySelector('body').classList.remove('active');
 };
